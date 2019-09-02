b2ef10aecf7ad6453352ec2e5a37ca27a3fe68b5
lpile <logan.pile@gmail.com>
2019-09-02 11:50:59 -0600

    Add Event model with spec
    Co-authored-by: Anneke McGrady <annekemcgrady@gmail.com>

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:event_time) }
+    it { should validate_presence_of(:creator) }
+  end
+end
