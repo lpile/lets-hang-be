f7ca92dc429c5b1300b4c183d51e28bde3fafbc2
lpile <logan.pile@gmail.com>
2019-09-02 13:30:23 -0600

    Add Faker to FactoryBot specs
    Co-authored-by: Anneke McGrady <annekemcgrady@gmail.com>

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :event do
-    title { "MyString" }
-    description { "MyText" }
-    event_time { "2019-09-02 11:29:56" }
-    creator { "MyString" }
+    title { Faker::Beer.style }
+    description { Faker::ChuckNorris.fact }
+    event_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short) }
+    creator { Faker::Games::Pokemon.name }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
-    phone_number { "MyString" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.safe_email }
+    password_digest { Faker::Internet.password }
+    phone_number { Faker::PhoneNumber.phone_number }
   end
 end
