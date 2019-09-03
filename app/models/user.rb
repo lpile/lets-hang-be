02438d03b00a8c700c68153cec3795a3697231e0
lpile <logan.pile@gmail.com>
2019-09-03 16:15:35 -0600

    Add friendship relationship to User model and spec
    Co-authored-by: Anneke McGrady <annekemcgrady@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+  # Sets up relationship between user and event
   has_many :user_events
   has_many :events, through: :user_events
 
+  # Sets up relationship between user and other users called friends
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+
+  # Validates model attributes
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, :first_name, :last_name, :phone_number, require: true
 
+  # For Bcrypt to hash user's password
   has_secure_password
 end
