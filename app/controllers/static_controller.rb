25911aedcd0938d0cfaa44f31f951500ee2cf62b
lpile <logan.pile@gmail.com>
2019-09-03 12:10:08 -0600

    Add comments for #index action for StaticController

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,10 @@ require 'rails/application_controller'
 
 class StaticController < Rails::ApplicationController
   def index
+    # Render Home Page
     render file: Rails.root.join('public', 'index.html')
+    # When '/' is hit, it points to the index.html in the public folder
+    # No need for route in config/routes.rb with Rails.root
+    # Unable to produce views when --api flag is used to create Rails app
   end
 end
