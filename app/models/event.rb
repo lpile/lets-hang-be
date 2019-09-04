7898cc2b1a39580f09e206d21adf2e149bb73b83
Anneke McGrady <annekemcgrady@gmail.com>
2019-09-04 16:19:23 -0600

    Add event_location column to Events table with specs

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   has_many :user_events
   has_many :users, through: :user_events
 
-  validates_presence_of :title, :description, :event_time, :creator
+  validates_presence_of :title, :description, :event_time, :creator, :event_location
 end
