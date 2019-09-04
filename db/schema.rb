c98308b42a2e7677b64bf1344535b034255226c5
lpile <logan.pile@gmail.com>
2019-09-04 13:40:19 -0600

    Add api_key column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_220609) do
+ActiveRecord::Schema.define(version: 2019_09_04_185744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_09_03_220609) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
   end
 
   add_foreign_key "friendships", "users"
