0899c1325884f61b379c8dd52f24d9b74c753fa0
Anneke McGrady <annekemcgrady@gmail.com>
2019-09-05 14:58:44 -0600

    Add create happy and sad paths to events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,4 +2,24 @@ class Api::V1::EventsController < ApplicationController
   def index
     render json: Event.all
   end
+
+  def create
+    user = User.find_by(api_key: params[:api_key])
+    if user
+      event = Event.new(event_params)
+      event.creator = "#{user.first_name} #{user.last_name}"
+      # binding.pry
+      if event.save
+        render json: EventSerializer.new(event), status: 201
+      else 
+        render json: { error: 'Failed to create event'}, status: 422
+      end
+    else 
+      render json: { error: 'Failed to find user' }, status: 404
+    end
+  end
+
+    def event_params
+      params.permit(:title, :description, :event_time, :event_location)
+  end
 end
