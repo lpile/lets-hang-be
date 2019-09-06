cd0fd3873284e7feb241c3addfd26090f6b4df0f
lpile <logan.pile@gmail.com>
2019-09-05 20:41:50 -0600

    Add :attendees to display all users who are attending the event

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -3,7 +3,12 @@ class EventSerializer
   set_type :event
   set_id :id
   attributes :title, :description, :creator, :event_location
+
   attribute :event_time do |object|
     Time.at(object.event_time).strftime('%I:%M%p %m/%d/%y')
+  end
+
+  attribute :attendees do |object|
+    object.users.map {|user| "#{user.first_name} #{user.last_name}"}
   end
 end
