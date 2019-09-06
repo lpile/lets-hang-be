eeb69b06eebe36b2c2e6e06bf145224ada4ad631
lpile <logan.pile@gmail.com>
2019-09-05 20:41:25 -0600

    Add #show action to EventsController with route

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,22 +3,43 @@ class Api::V1::EventsController < ApplicationController
     render json: Event.all
   end
 
+  def show
+    user = User.find_by(api_key: params[:api_key])
+    if user
+      event = Event.find_by(id: params[:id])
+      if event
+        if event.creator == "#{user.first_name} #{user.last_name}"
+          render json: EventSerializer.new(event), status: 200
+        else
+          render json: { error: 'Unauthorized user' }, status: 401
+        end
+      else
+        render json: { error: 'Failed to find event' }, status: 404
+      end
+    else
+      render json: { error: 'Failed to find user' }, status: 404
+    end
+  end
+
   def create
     user = User.find_by(api_key: params[:api_key])
     if user
       event = Event.new(event_params)
       event.creator = "#{user.first_name} #{user.last_name}"
       if event.save
+        UserEvent.create(user: user, event: event)
         render json: EventSerializer.new(event), status: 201
-      else 
+      else
         render json: { error: 'Failed to create event'}, status: 422
       end
-    else 
+    else
       render json: { error: 'Failed to find user' }, status: 404
     end
   end
 
-    def event_params
-      params.permit(:title, :description, :event_time, :event_location)
+  private
+
+  def event_params
+    params.permit(:title, :description, :event_time, :event_location)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # All events
-      resources :events, only: [:index, :create]
+      resources :events, only: [:index, :show, :create]
 
       # All users
       resources :users, only: [:create]
@@ -11,12 +11,10 @@ Rails.application.routes.draw do
       namespace :user do
         resources :events, only: [:index]
       end
+      patch 'user/edit', to: 'users#update'
 
       # User Login
       post '/sessions', to: 'sessions#create'
-
-      # Edit User
-      patch 'user/edit', to: 'users#update'
     end
   end
 end
