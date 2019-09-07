31d0df9c173be5ebbec150cefa55f90275652075
Anneke McGrady <annekemcgrady@gmail.com>
2019-09-07 16:26:18 -0600

    Add status column to user_events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_221325) do
+ActiveRecord::Schema.define(version: 2019_09_07_205313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_221325) do
   create_table "user_events", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "event_id"
+    t.integer "status"
     t.index ["event_id"], name: "index_user_events_on_event_id"
     t.index ["user_id"], name: "index_user_events_on_user_id"
   end
