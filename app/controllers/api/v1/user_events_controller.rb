916d06d8736ed5bcd38721370256051596bfeb33
Anneke McGrady <annekemcgrady@gmail.com>
2019-09-08 16:23:31 -0600

    Migrate and Seed, add user_event/create info to README

--- app/controllers/api/v1/user_events_controller.rb
+++ app/controllers/api/v1/user_events_controller.rb
@@ -15,21 +15,4 @@ class Api::V1::UserEventsController < ApplicationController
     end
   end
 
-
-#Invited user accepting 
-def update
-  user = User.find_by(api_key: params[:api_key])
-    if user
-      event = Event.find_by(id: params[:event_id])
-      if event 
-        user_event = UserEvent.find_by(event_id: params[:event_id], status: [:pending])&.accepted!
-        render json: EventInviteSerializer.new(user_event), status: 202
-      else 
-        render json: { error: 'Failed to find event' }, status: 404
-      end
-    else
-      render json: { error: 'Failed to find user' }, status: 404
-    end
-  end
-
 end
