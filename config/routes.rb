00949eabf43f6c31e35ce65e2890f3f1f3cf2e06
Anneke McGrady <annekemcgrady@gmail.com>
2019-09-08 15:13:33 -0600

    Refactor user_event path to convention

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,22 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # All events
-      resources :events, only: [:show, :create, :update]
+      resources :events, only: [:index, :show, :create]
+
+      # Update an event
+      patch 'events/:id/edit', to: 'events#update'
 
       # All users
-      resources :users, only: [:create, :update]
+      resources :users, only: [:create]
 
-      # All friendships
-      resources :friendships, only: [:create, :update, :destroy]
+      # User Events - invite, accept, decline
+      resources :user_events, only: [:create, :update, :destroy]
 
       # Specific User
       namespace :user do
         resources :events, only: [:index]
-        resources :friends, only: [:index]
       end
-
-      # UserEvents upon inviting, accepting, declining event invites
-      namespace :event do
-        resources :user_events, only: [:create, :update, :destroy]
-      end
+      patch 'user/edit', to: 'users#update'
 
       # User Login
       post '/sessions', to: 'sessions#create'
