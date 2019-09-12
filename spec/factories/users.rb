71499ae8ec3d321b4d57adfb42273c038bca3f3d
lpile <logan.pile@gmail.com>
2019-09-11 18:51:03 -0600

    Update seed file for demo

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.unique.safe_email }
-    password { Faker::Internet.password }
+    password { 'password' }
     phone_number { Faker::PhoneNumber.cell_phone }
     api_key { Faker::Crypto.md5 }
   end
