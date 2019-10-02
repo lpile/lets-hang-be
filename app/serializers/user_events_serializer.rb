783460fdf9c1a10de8eb5587f98341bcbaa99175
Anneke McGrady <annekemcgrady@gmail.com>
2019-10-02 12:38:30 -0600

    Update user_events_serializer to included attending users

--- app/serializers/user_events_serializer.rb
+++ app/serializers/user_events_serializer.rb
@@ -4,13 +4,15 @@ class UserEventsSerializer
   set_id :id
   attribute :events do |object|
     object.events.map do |event|
+      # binding.pry
       {
        "id": event.id,
        "title": event.title,
        "description": event.description,
        "event_time": event.event_time,
        "event_location": event.event_location,
-       "creator": event.creator
+       "creator": event.creator,
+       "attending": event.accepts.map { |user| user.first_name + ' ' + user.last_name }
       }
     end
   end
